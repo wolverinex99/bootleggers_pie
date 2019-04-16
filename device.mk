369c4b84eb329c4728feaa38bad9b460b4006009
Ritesh Saxena <riteshsax007@gmail.com>
2019-04-16 13:00:04 +0000

    Revert "land: Reloaded-CAF Bringup"
    This reverts commit fc57a6114c80e170a4d5963657d8e855d429f2f8.
    
    Change-Id: Iec1092b5404d14c1239a7eb65022c91a104f261e

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ $(call inherit-product, vendor/xiaomi/land/land-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
